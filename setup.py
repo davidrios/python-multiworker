10c57106580a8de8f311b941f3f328aa63b84848
David Rios <david.rios@upessencia.com.br>
2014-02-25 11:30:05 -0300

    added python package stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='multiworker',
+    version='0.1',
+    description='Abstraction for a threading multiworker job processor.',
+    author='David Rios',
+    author_email='david.rios.gomes@gmail.com',
+    py_modules=['multiworker'],
+)
