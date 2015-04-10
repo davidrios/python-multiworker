5ad633d103c0f94aadb899bcf945da9feb3dc6a5
David Rios <david.rios@upessencia.com.br>
2015-04-10 17:16:34 -0300

    bump minor release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='multiworker',
-    version='0.1.1',
+    version='0.1.2',
     description='Abstraction for a threading multiworker job processor.',
     author='David Rios',
     author_email='david.rios.gomes@gmail.com',
